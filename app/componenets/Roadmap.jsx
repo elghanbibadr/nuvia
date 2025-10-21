f914102db8408c3eef2ec33f63a588d97d147f6f
elghanbibadr <bghanbi50@gmail.com>
2025-10-21 22:02:40 +0100

    translate the roadmap

--- app/componenets/Roadmap.jsx
+++ app/componenets/Roadmap.jsx
@@ -2,14 +2,31 @@ import Image from 'next/image'
 import React from 'react'
 import roadmapMobile from "@/public/roadmapMobile.svg"
 import roadmapDesktop from "@/public/roadmpaDesktop.svg"
+import roadmapDesktopestonian from "@/public/roadmapDesktopestonian.svg"
+import roadmapMobileestonian from "@/public/roadmapMobileestonian.svg"
+
+import { useLocale } from 'next-intl'
+
+
+
 
 
 
 const Roadmap = () => {
+      const locale = useLocale(); // This gets the current locale (en or et)
+  
   return (
     <div id='roadmap' className='my-20 '>
-   <Image height={400} width={400} src={roadmapMobile}  className="md:hidden" alt='' />
+
+  { locale==="es" ? <>
+   <Image height={400} width={400} src={roadmapMobileestonian}  className="md:hidden" alt='' />
+   <Image height={900} width={900} src={roadmapDesktopestonian}  className="hidden w-full md:block" alt='' />
+  </>
+   : <>
+     <Image height={400} width={400} src={roadmapMobile}  className="md:hidden" alt='' />
    <Image height={900} width={900} src={roadmapDesktop}  className="hidden w-full md:block" alt='' />
+   </>
+   }
 
 
   
